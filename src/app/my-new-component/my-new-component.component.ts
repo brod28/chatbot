309640cf5a8655d942b7fca2bb6002e49421613d
Dima Brodsky <Dima Brodsky>
2017-09-22 17:25:38 +0400

    sad

--- src/app/my-new-component/my-new-component.component.ts
+++ src/app/my-new-component/my-new-component.component.ts
@@ -35,11 +35,44 @@ export class MyNewComponentComponent implements OnInit {
  
 
   SubmitRequest(box_value){
-
+    let retVal;
     // show the message on screen
     this.request=box_value;
     this.messages.push({message:this.request,type:"me"});
     console.log('request = ' + this.request);
+
+    if(box_value=="I am from Berlin"){
+       retVal= "ready";
+    }
+    else if(box_value=="ready"){
+       retVal= "I want to learn about Moorgate";
+    }
+    else if(box_value=="I want to learn about Moorgate"){
+       retVal= "I want to stay room";
+    }
+    else if(box_value=="I want to stay room"){
+       retVal= "add";
+    }
+    else if(box_value=="add"){
+       retVal= "commuting";
+    }
+    else if(box_value=="commuting"){
+       retVal= "tube";
+    }
+    else if(box_value=="tube"){
+       retVal= "I am happy with this choice";
+    }
+    else if(box_value=="I am happy with this choice"){
+       retVal= "2 times a week";
+    }
+    else if(box_value=="2 times a week"){
+       retVal= "no";
+    } 
+    else {
+       retVal= "";      
+    }
+      
+
 
     // send message to API AI and handle the response
     this.client.textRequest(this.request)
@@ -50,18 +83,24 @@ export class MyNewComponentComponent implements OnInit {
       console.log(error);
       this.response="sorry but something went wrong, try again in few minutes";
     })
+    return retVal;   
   }
  
   handleAPIAIresponse(response){
+    console.log(response);
     this.response=response.result.fulfillment.speech;
     let responsemessage=this.response.split("$$$");
     responsemessage.forEach(element => {
-      this.messages.push({message:element,type:"agent"});
+      if(element!=""){
+        this.messages.push({message:element,type:"agent"});
+      }
     });
     this.readings(response);
     this.ScrollDown();    
     
     let fire_event=response.result.fulfillment.data.fire_event;
+    
+    // if we want to fire specific event
     if(fire_event!=""){
       this.client.eventRequest(fire_event)
       .then((response) => {
@@ -72,7 +111,7 @@ export class MyNewComponentComponent implements OnInit {
         this.response="sorry but something went wrong, try again in few minutes";
       })
     }
-    console.log(this.messages);    
+    console.log(this.messages); 
   }
 
   ScrollDown() {
